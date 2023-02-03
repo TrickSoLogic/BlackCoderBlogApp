377ed2add95a95aaa8c71de961c7c47c665b4db3
TrickSoLogic <118069084+TrickSoLogic@users.noreply.github.com>
2023-02-04 02:19:42 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlackCoderBlogApp
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
